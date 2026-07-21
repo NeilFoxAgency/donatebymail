57959b8d9a94748c7e689e06d98056f4e1fa0c1d
Neil Fox <166657561+NeilFoxAgency@users.noreply.github.com>
2026-07-21 14:20:24 -0400

    design: load documentary photography layer

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 import './human-refresh.css';
+import './photo-refresh.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
