c126b62e5a8283b53f015a6cad7483069d954267
Neil Fox <166657561+NeilFoxAgency@users.noreply.github.com>
2026-07-21 23:12:49 -0400

    types: define Pledge Vite environment variables

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_PLEDGE_PARTNER_KEY?: string;
+  readonly VITE_PLEDGE_ENV?: 'production' | 'sandbox';
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
